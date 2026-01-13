2ff2ce3513d4035caa603596a09076e7e0528122
SAKETH11111 <sakethbaddam10@gmail.com>
2026-01-13 16:04:56 -0600

    Refactor data module exports to include new PFAS OOD splitting utilities and enhance scaffold splitting functionality

--- src/gnn/data/splits/__init__.py
+++ src/gnn/data/splits/__init__.py
@@ -1,9 +1,15 @@
 """Data splitting utilities."""
 
+from .pfas_ood import get_chain_length, get_headgroup, pfas_ood_split
 from .scaffold import get_scaffold, group_by_scaffold, scaffold_split
 
 __all__ = [
+    # Scaffold splitting
     "get_scaffold",
     "group_by_scaffold",
     "scaffold_split",
+    # PFAS OOD splitting
+    "get_chain_length",
+    "get_headgroup",
+    "pfas_ood_split",
 ]
