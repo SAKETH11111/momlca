d88eb1f1e4ba730ab8ec922fb9167f36f6809adf
SAKETH11111 <sakethbaddam10@gmail.com>
2026-01-13 14:40:57 -0600

    Add data splitting utilities and update exports in data module

---
+++ src/gnn/data/splits/__init__.py
@@ -0,0 +1,9 @@
+"""Data splitting utilities."""
+
+from .scaffold import get_scaffold, group_by_scaffold, scaffold_split
+
+__all__ = [
+    "get_scaffold",
+    "group_by_scaffold",
+    "scaffold_split",
+]
